69444c4dbe7100e259494d345a95792651156ad2
Duyrep <131185997+Duyrep@users.noreply.github.com>
2024-12-06 22:32:48 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,6 @@
 export default function Home() {
   return (
     <div>
-      <div>
-        
-      </div>
-      <input className="pl-2 border border-black rounded-md" type="text" />
     </div>
   );
 }
